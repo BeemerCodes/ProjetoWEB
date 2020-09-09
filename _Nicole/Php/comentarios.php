d63b61f290bdc958a906d0fd9da627aabb491fea
Nicole <nicoleapruivo@gmail.com>
2020-09-09 13:42:29 -0300

    Php - Inicio do curso

---
+++ _Nicole/Php/comentarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios</title>
+</head>
+
+<body>
+
+    <?php 
+    #comentario de uma linha
+    
+        echo 'Esta e uma tag longa'
+    ?>
+ <br>
+    <?= 
+    //comentario de uma linha
+    'Esta e uma tag de impressao'
+    ?>
+    <br>
+    <?
+    /*comentario de multiplas linhas*/
+     echo 'Esta e uma tag curta'
+     ?>
+
+    
+</body>
+</html>
---
+++ _Nicole/Php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php 
+        echo 'Esta e uma tag longa'
+    ?>
+ <br>
+    <?= 'Esta e uma tag de impressao'?>
+    <br>
+    <? echo 'Esta e uma tag curta'?>
+
+    
+</body>
+</html>
