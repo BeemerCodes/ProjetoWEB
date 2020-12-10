35b205db2e2a95867e2f9bb8506aa5d9995c9f7a
Nicole <nicoleapruivo@gmail.com>
2020-12-10 16:56:44 -0300

    Ajuste senha SMTP

--- _Nicole/Php/app_send_mail/arq_seguro/processa_envio.php
+++ _Nicole/Php/app_send_mail/arq_seguro/processa_envio.php
@@ -58,7 +58,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                    // Enable SMTP authentication
     $mail->Username   = 'thehostnotfound@gmail.com';             // SMTP username
-    $mail->Password   = '303030nr';                              // SMTP password
+    $mail->Password   = ' ';                                     // SMTP password      //senha adicionada quando rodar
     $mail->SMTPSecure = 'tsl';                                   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                     // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
